5dacd9483ba78f35610d0e8f937cf1ca72b62754
Kristi Eaglin <kristi_auzenne@yahoo.com>
2023-05-18 10:31:36 -0500

    random button, js complete

--- script.js
+++ script.js
@@ -7,21 +7,42 @@ const premier = document.getElementById("premier");
 const ended = document.getElementById("ended");
 const summary = document.getElementById("summary");
 const rating = document.getElementById("rating");
+const pickShowButton = document.getElementById("pick-show-button");
 
-fetch("https://api.tvmaze.com/shows/80")
-    .then((res) => res.json())
-    .then((json) => {
-        const tvShow = json;
-        console.log(tvShow);
-        const showPic = json.image.original;
-        showPicContaioner.setAttribute("src", showPic);
-        showName.innerText = tvShow.name;
-        runtime.innerText = "Runtime: " + tvShow.averageRuntime;
-        genre.innerText = "Genre: " + tvShow.genres;
-        network.innerText = "Network: " + tvShow.network.name;
-        premier.innerText = "Premiered: " + tvShow.premiered;
-        ended.innerText = "Ended: " + tvShow.ended;
-        summary.innerHTML = "Summary: " + tvShow.summary;
-        rating.innerText = "Rating: " + tvShow.rating.average;
-    })
-    .catch((err) => console.log(err));
+function elementHTML(el, text){
+    el.innerHTML = text;
+}
+
+function populateShowInfo(tvShow){
+    elementHTML(showName, tvShow.name);
+    elementHTML(runtime, `<b>Runtime:</b> ${tvShow.averageRuntime} minutes`);
+    elementHTML(genre, `<b>Genre:</b> ${tvShow.genres.join(", ")}`);
+    elementHTML(network, `<b>Network:</b> ${tvShow.network.name}`);
+    elementHTML(premier, `<b>Premiered:</b> ${tvShow.premiered}`);
+    elementHTML(ended, `<b>Ended:</b> ${tvShow.ended}`);
+    elementHTML(rating, `<b>Rating:</b> ${tvShow.rating.average}`);
+    elementHTML(summary, tvShow.summary);
+}
+
+function createShowDescription(tvShow){
+    const showPic = tvShow.image.original;
+    showPicContaioner.setAttribute("src", showPic);
+    populateShowInfo(tvShow);
+}
+
+function getTvShow(){
+    const randomTvShow = Math.floor(Math.random()*249 + 1);
+    fetch(`https://api.tvmaze.com/shows/${randomTvShow}`)
+        .then((res) => res.json())
+        .then((json) => {
+            const tvShow = json;
+            createShowDescription(tvShow);
+        })
+        .catch((err) => console.log(err));
+}
+
+getTvShow();
+
+pickShowButton.addEventListener("click", function(){
+    getTvShow();
+});
